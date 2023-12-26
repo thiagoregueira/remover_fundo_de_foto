ce1f804a4e497c82b5718fd3f015e9d85f971857
Thiago Regueira <thiago.regueira@yahoo.com.br>
2023-12-26 10:20:15 -0300

    adicionando o botão para download

--- remover_fundo_foto.py
+++ remover_fundo_foto.py
@@ -1,13 +1,27 @@
 import streamlit as st
 from PIL import Image
 from rembg import remove
+import io
 
 
-def remover_fundo(foto, widget):
+def remover_fundo(foto, col1, col2):
     img = Image.open(foto)
     output = remove(img)
-    widget.title("Foto sem fundo")
-    widget.image(output)
+    col2.title("Foto sem fundo")
+    col2.image(output)
+
+    # Salvar a imagem sem fundo em um buffer de bytes
+    output_buffer = io.BytesIO()
+    output.save(output_buffer, format="PNG")
+    output_buffer.seek(0)
+
+    # Adicionar o botão de download abaixo da foto sem fundo
+    col2.download_button(
+        label="Download Imagem sem fundo",
+        data=output_buffer,
+        file_name="imagem_sem_fundo.png",
+        mime="image/png",
+    )
 
 
 st.set_page_config(page_title="Remover fundo de fotos", page_icon="📷", layout="wide")
@@ -16,10 +30,11 @@ st.title("Removendo fundo de fotos")
 
 foto = st.file_uploader("Escolha uma foto", type=["png", "jpg", "jpeg"])
 
-
 col1, col2 = st.columns(2)
 
 if foto is not None:
     col1.title("Foto Original")
     col1.image(foto)
-    st.button("Remover fundo", on_click=remover_fundo, args=(foto, col2))
+
+    if col1.button("Remover fundo"):
+        remover_fundo(foto, col1, col2)
