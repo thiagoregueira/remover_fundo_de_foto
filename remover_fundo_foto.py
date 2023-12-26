96a914264d8be06af4d2375b059a341a4ca63805
Thiago Regueira <thiago.regueira@yahoo.com.br>
2023-12-25 21:04:08 -0300

    app de remoção de fundo de foto

---
+++ remover_fundo_foto.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from PIL import Image
+from rembg import remove
+
+
+def remover_fundo(foto, widget):
+    img = Image.open(foto)
+    output = remove(img)
+    widget.title("Foto sem fundo")
+    widget.image(output)
+
+
+st.title("Removendo fundo de fotos")
+
+foto = st.file_uploader("Escolha uma foto", type=["png", "jpg", "jpeg"])
+
+
+col1, col2 = st.columns(2)
+
+if foto is not None:
+    col1.title("Foto Original")
+    col1.image(foto)
+    st.button("Remover fundo", on_click=remover_fundo, args=(foto, col2))
